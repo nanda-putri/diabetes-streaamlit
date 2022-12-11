9f13289cee4932b00bb913acac6af96111f77419
nanda-putri <83993128+nanda-putri@users.noreply.github.com>
2022-12-11 17:24:26 +0700

    update

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ elif choose == "Diagnosis":
         y = data['Outcome'].values
 
         # split datasets
-        st.subheader("Split Datasets")
+        st.header("Split Datasets")
         from sklearn.model_selection import train_test_split
         x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 1) 
 
